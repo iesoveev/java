f62eac155865b62590b8f336389e8305a710a272
iesoveev <iesoveev@gmail.com>
2018-08-02 23:52:49 +0300

    banarySearch commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new JavaMethods().nod(0, 0));
+        System.out.println(new JavaMethods().binarySearch(new int[]{1, 2, 3, 4, 5, 6, 7}, 3));
 
     }
 }
