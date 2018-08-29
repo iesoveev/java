9b38d9de94fd473882aa429de876fc6de397ccdc
iesoveev <iesoveev@gmail.com>
2018-08-30 02:01:10 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 import java.io.*;
 import java.util.Arrays;
+import java.util.Scanner;
 
 
 public class Main {
-
-
-    public static void main(String[] args) throws IOException {
-        int[] arr = new int[]{2, 3, 6, 10, 13, 22, 31, 310};
-        int[] arr1 = new int[]{1, 4, 5, 6, 7, 8, 9 ,21};
-        System.out.println(Arrays.toString(new MergeSort().merge(arr, arr1)));
-
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scn.nextInt();
+        }
+        int[] sortedA = new MergeSort().mergeSort(a);
+        System.out.println(Arrays.toString(sortedA));
 
 
     }
--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,28 +1,49 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MergeSort {
-    public int[] merge(int[] left, int[] right) {
-        int[] result = new int[left.length + right.length];
+
+
+    public int[] merge(int[] arr1, int[] arr2) {
+        int[] result = new int[arr1.length + arr2.length];
+
+
         int i = 0;
         int j = 0;
+
         for (int k = 0; k < result.length; k++) {
-            if (i > left.length - 1) {
-                result[k] = right[j];
+            if (i > arr1.length - 1) {
+                result[k] = arr2[j];
                 j++;
-            } else if (j > right.length - 1) {
-                result[k] = left[i];
+            } else if (j > arr2.length - 1) {
+                result[k] = arr1[i];
                 i++;
-            } else if (left[i] >= right[j]) {
-                result[k] = right[j];
-                j++;
+            } else if (arr1[i] <= arr2[j]) {
+                result[k] = arr1[i];
+                i++;
             } else {
-                result[k] = left[i];
-                i++;
+                result[k] = arr2[j];
+                j++;
             }
         }
         return result;
     }
 
+
+    public int[] mergeSort(int[] arr) {
+        if (arr.length == 1) return arr;
+        int n = arr.length;
+        int m = n / 2;
+        int[] left = new int[m];
+        int[] right = new int[n - m];
+        System.arraycopy(arr, 0, left, 0, m);
+        System.arraycopy(arr, m, right, 0, n - m);
+        left = mergeSort(left);
+        right = mergeSort(right);
+        return merge(left, right);
+    }
+
+
 }
-
-
-
-
